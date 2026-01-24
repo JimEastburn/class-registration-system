b8b2cc7c1a85dcf728a88da6c8d274164a32e1fb
JimEastburn <eastburn.jim@gmail.com>
2026-01-24 13:03:26 -0600

    added loading spinners to all parent routes

---
+++ src/app/(dashboard)/parent/family/loading.tsx
@@ -0,0 +1,9 @@
+import { LoadingSpinner } from "@/components/ui/loading-spinner";
+
+export default function Loading() {
+    return (
+        <div className="flex h-full w-full items-center justify-center p-8">
+            <LoadingSpinner className="h-8 w-8 text-primary" />
+        </div>
+    );
+}
